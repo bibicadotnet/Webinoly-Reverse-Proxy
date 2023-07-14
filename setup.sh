fb43cf6ce90b57d9f7b0b05d9145a95148a4575e
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-14 09:54:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,8 @@ sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
 
+sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
 sudo apt update && sudo apt upgrade -y
 sudo webinoly -verify
 sudo webinoly -info
